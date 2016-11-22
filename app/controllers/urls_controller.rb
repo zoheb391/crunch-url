28eea50939d6db9216d779fbc5021aefaa6861a6
Zoheb Salam <zoheb_391@hotmail.com>
2016-11-22 23:29:41 +0000

    simple form + url_validations

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,4 +2,24 @@ class UrlsController < ApplicationController
   def index
     @urls = Url.all
   end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.create(url_params)
+    if @url.valid?
+      redirect_to root_path
+    else 
+      render :new, :status => :unprocessable_entity
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:target)
+  end
+
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,5 @@
 class Url < ActiveRecord::Base
+  belongs_to :user
+
+  validates :target, :length => { minimum:5 }, :format => URI::regexp(%w(http https)), :presence => true
 end
